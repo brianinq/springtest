0ae5059315904e20e6093de20671a6b0831f94f3
Brian Kipkoech <kipkoechbrian0@gmail.com>
2024-03-11 09:21:19 +0300

    find by id tests

--- src/test/java/com/example/testing/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/testing/repository/EmployeeRepositoryTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTest {
     @Autowired
@@ -34,18 +37,52 @@ public class EmployeeRepositoryTest {
     }
 
     @Test
-    @DisplayName("Test find Employee by Id")
-    public void givenEmployee_whenFindByID_thenReturnEmployee(){
+    @DisplayName("Test findAll Employees")
+    public void givenEmployees_whenFindAll_thenReturnEmployeeList(){
+        Employee employee = Employee
+                .builder()
+                .firstName("Benson")
+                .lastName("Astrakhan")
+                .email("e@h.v")
+                .build();
+        Employee employee1 = Employee
+                .builder()
+                .firstName("Alex")
+                .lastName("Given")
+                .email("geel@gnk.com")
+                .build();
+        employeeRepository.save(employee);
+        employeeRepository.save(employee1);
+
+        //when
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        // then
+        Assertions.assertThat(employeeList).isNotNull();
+        Assertions.assertThat(employeeList.size()).isEqualTo(2);
 
     }
 
-    @Test
-    @DisplayName("Name to Display")
-    public void given_when_then(){
-        //given - precondition / setup
 
-        //when - action or behaviour that we are testing
 
-        //then - verify the output
-    }
+     @Test
+     @DisplayName("Find Employee By Id")
+     public void givenEmployee_whenFindById_thenReturnEmployee(){
+         //given - precondition / setup
+         Employee employee = Employee
+                 .builder()
+                 .firstName("Benson")
+                 .lastName("Astrakhan")
+                 .email("e@h.v")
+                 .build();
+         employeeRepository.save(employee);
+
+         //when
+         Employee savedEmployee = employeeRepository.findById(1L).get();
+
+         //then - verify the output
+         Assertions.assertThat(savedEmployee).isNotNull();
+     }
+
+
 }
